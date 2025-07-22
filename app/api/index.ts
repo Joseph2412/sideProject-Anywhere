1e72321dfde22c8cb62b43135eaffc3a9b1921f4
Joseph2412 <63360070+Joseph2412@users.noreply.github.com>
2025-07-22 05:52:48 +0200

    Adding Silly Comments + Setting FE <-> BE Integration

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,9 +1,22 @@
 import fastify from 'fastify'
 
 const server = fastify()
+//To Do
+//Esporre un endpoint GET /ping su BE, l'endooint legge un parametro t in queryString e ritorna un JSON
+// {
+//   "value": t
+// }
+// Sul front effettuare una chiamata alla GET /ping?t=xxx, aspettare la risposta e mostrare a schermo il valore tornato da value
+// Fare in modo che la chiamata punti all'ambiente di sviluppo corretto in base allo script lanciato
 
-server.get('/funge', async (request, reply) => {
-  return 'FUNONZIAAAAAAA'
+//Idea: Modifico con queryString => richiamo il value Corretto t: tipo Stringa
+//Se value corretto => risposta 200?
+//Ritorno tipo JSON e mando value : t
+//Ho scritto una cazzata? Forse
+
+
+server.get('/ping', async (request, reply) => {
+  //return 'pong' test con curl corretto
 })
 
 server.listen({ port: 3001 }, (err, address) => {
