2f6c2f4890c09da01807f636da836af77909ab5f
Joseph2412 <random47@hotmail.it>
2025-07-23 17:58:37 +0200

    Creata Repo Da Zero

---
+++ apps/host/theme/theme.ts
@@ -0,0 +1,18 @@
+//Theme Casuale Gentilmente offerto dalla documentazione di Ant Design
+// app/host/theme/theme.ts
+import type { ThemeConfig } from 'antd'
+
+const theme: ThemeConfig = {
+   
+      token: {
+        // Seed Token
+        colorPrimary: '#00b96b',
+        borderRadius: 2,
+
+        // Alias Token
+        colorBgContainer: '#f6ffed',
+      },
+    
+}
+
+export default theme
